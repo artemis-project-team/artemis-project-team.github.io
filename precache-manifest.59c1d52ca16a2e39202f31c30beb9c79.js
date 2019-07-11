b2af5c98dd1cc4d010ea3afcb1868a57ea84087e
arthur2007 <arthur07.gao@gmail.com>
2019-07-11 21:08:10 +0800

    init commit.

---
+++ precache-manifest.59c1d52ca16a2e39202f31c30beb9c79.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea4c2abb5efe8305b16f4402ef28e4f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b188dcab48e0bb998595",
+    "url": "/static/css/2.7893baac.chunk.css"
+  },
+  {
+    "revision": "d91abf7870917f9b72e5",
+    "url": "/static/css/main.ce0082b1.chunk.css"
+  },
+  {
+    "revision": "b188dcab48e0bb998595",
+    "url": "/static/js/2.085120df.chunk.js"
+  },
+  {
+    "revision": "d91abf7870917f9b72e5",
+    "url": "/static/js/main.10209f18.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "6518be9677f077b336957fa588b6c9c1",
+    "url": "/static/media/artemis_logo2.6518be96.png"
+  },
+  {
+    "revision": "51a470c654a93908f156420e751c6885",
+    "url": "/static/media/banner2.51a470c6.png"
+  }
+]);
